82c3c09fef6f9366966fafef245a30d350c15744
Mario-Hee <icon94@naver.com>
2020-03-24 11:29:06 +0900

    자유게시판 - 상세게시물 Session을 활용한 조회수 무한증가막기(24시간안에 1번씩 증가 제한)

--- creme/src/main/java/com/creme/service/Board/BoardServiceImpl.java
+++ creme/src/main/java/com/creme/service/Board/BoardServiceImpl.java
@@ -58,9 +58,31 @@ public class BoardServiceImpl implements BoardService{
 
 	@Override
 	public void increaseViewCnt(int bno, HttpSession session) {
-		// 조회수 +1 증가
-		bDao.increaseViewCnt(bno);
+		// ex) now_time: 현재시간
+		// ex) plus_time: 조회수 + 1 시간
+		// now_time - plus_time = 조회수 + 1 증가 후 지난시간
 		
+		long update_time = 0; // 조회수 + 1 증가한 시간
+		
+		if(session.getAttribute("update_time_"+bno) != null) {  // "update_time"+bno은 세션에 아직 값이 없으므로 null, !=null과 값이 같으므로 if 안탐
+			//게시물 bno를 210이라 할때,
+			//update_time_"+bno => update_time_210
+			
+			// 최근에 조회수를 올린 시간
+			update_time = (long)session.getAttribute("update_time_"+bno);
+		}
+		
+		// 현재시간
+		long current_time = System.currentTimeMillis(); // 35000초, 시간 초
+		
+		// 일정시간이 경과한 후 조회수 증가 처리
+		//if(current_time - update_time > 5000) { 			 // 새로고침 후 5초후에 조회수 +1 증가
+		if(current_time - update_time > 24*60*60*1000) {	 // 하루마다 조회수 +1 증가, 1day
+			// DB에서 조회수 +1 증가
+			bDao.increaseViewCnt(bno);
+			// 조회수 올린 시간 저장
+			session.setAttribute("update_time_"+bno, current_time);
+		}
 	}
 	
 	
